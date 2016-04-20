7fd907b401a88b61eb0af6ab9fe6eeb4d1efd688
sunhong <sunhong@ucfgroup.com>
2016-04-20 10:30:45 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by IOS-Sun on 16/4/20.
+//  Copyright © 2016年 IOS-Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
